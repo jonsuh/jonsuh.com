b0d2c1a677a5eae2d2fd3e7d1da979cbea227d3b
Jonathan Suh <hello@jonsuh.com>
2015-02-18 13:28:36 -0600

    Better font and script loading.

--- assets/js/src/critical.js
+++ assets/js/src/critical.js
@@ -38,10 +38,38 @@ function loadJS( src, cb ){
   return script;
 }
 
+(function(d) {
+  var config = {
+      kitId: "iko7pqe",
+      scriptTimeout: 3000
+    },
+    h = d.documentElement,
+    t = setTimeout(function() {
+      h.className = h.className.replace(/\bwf-loading\b/g, "") + " wf-inactive";
+    }, config.scriptTimeout),
+    tk = d.createElement("script"),
+    f = false,
+    s = d.getElementsByTagName("script")[0],
+    a;
+  h.className += " wf-loading";
+  tk.src = "//use.typekit.net/" + config.kitId + ".js";
+  tk.async = true;
+  tk.onload = tk.onreadystatechange = function() {
+    a = this.readyState;
+    if (f || a && a != "complete" && a != "loaded") return;
+    f = true;
+    clearTimeout(t);
+    try {
+      Typekit.load(config)
+    } catch (e) {}
+  };
+  s.parentNode.insertBefore(tk, s)
+})(document);
+
 var metaTags     = document.getElementsByTagName("meta"),
     metaArray    = [],
     // metaContent  = ["fontsCSS", "fontsJS", "mainCSS", "mainJS", "pageCSS", "pageJS"];
-    metaContent  = ["fontsCSS", "fontsJS", "pageCSS"];
+    metaContent  = ["pageCSS"];
 
 for (var i = 0; i < metaTags.length; i++) {
   if (metaContent.indexOf(metaTags[i].getAttribute("name")) > -1) {
