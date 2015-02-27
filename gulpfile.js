6a37db4cb04500a52514b199b5d5e82a2c22c9f2
Jonathan Suh <hello@jonsuh.com>
2015-02-27 09:55:42 -0600

    Gulp jekyll tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp         = require('gulp'),
     browserSync  = require('browser-sync'),
     filter       = require('gulp-filter'),
     reload       = browserSync.reload,
+    shell        = require('gulp-shell'),
     sass         = require('gulp-sass');
 
 gulp.task('sass', function() {
@@ -19,14 +20,41 @@ gulp.task('sass', function() {
       browsers: ['last 2 versions'],
     }))
     // .pipe(sourcemaps.write())
-    .pipe(gulp.dest('_site/assets/css'))
     .pipe(gulp.dest('assets/css'))
     .pipe(filter('**/*.css'))
     .pipe(reload({stream: true}));
 });
 
+gulp.task('sass-jekyll', function() {
+  return gulp.src('assets/sass/**/*.scss')
+    // .pipe(sourcemaps.init())
+    .pipe(sass({
+      includePaths: [
+        'bower_components/bourbon/app/assets/stylesheets',
+        'bower_components/mq-sass/stylesheets',
+        'bower_components/neat/app/assets/stylesheets'
+      ]
+    }))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+    }))
+    // .pipe(sourcemaps.write())
+    .pipe(gulp.dest('_site/assets/css'))
+    .pipe(filter('**/*.css'))
+    .pipe(reload({stream: true}));
+});
+
+gulp.task('jekyll', shell.task([
+  'bundle exec jekyll build',
+]));
+
+gulp.task('jekyll-watch', shell.task([
+  'bundle exec jekyll build --watch',
+]));
+
 gulp.task('watch', function() {
-  gulp.watch('assets/sass/**/*.scss', ['sass']);
+  gulp.watch('assets/sass/**/*.scss', ['sass-jekyll']);
+  gulp.run('jekyll-watch');
 });
 
 gulp.task('browsersync', function() {
@@ -37,6 +65,6 @@ gulp.task('browsersync', function() {
   });
 });
 
-gulp.task('build', ['sass']);
+gulp.task('build', ['sass', 'jekyll']);
 
-gulp.task('default', ['build', 'browsersync', 'watch']);
+gulp.task('default', ['sass', 'browsersync', 'watch']);
