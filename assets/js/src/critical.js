7d004be47070dd2d54ba83e091744d8f2d90a5f7
Jonathan Suh <hello@jonsuh.com>
2015-03-27 11:35:01 -0500

    Remove loadCSS from critical.js.

--- assets/js/src/critical.js
+++ assets/js/src/critical.js
@@ -1,30 +1,3 @@
-function loadCSS( href, before, media ){
-  "use strict";
-  var ss = window.document.createElement( "link" );
-  var ref = before || window.document.getElementsByTagName( "script" )[ 0 ];
-  var sheets = window.document.styleSheets;
-  ss.rel = "stylesheet";
-  ss.href = href;
-  ss.media = "only x";
-  ref.parentNode.insertBefore( ss, ref );
-  function toggleMedia(){
-    var defined;
-    for( var i = 0; i < sheets.length; i++ ){
-      if( sheets[ i ].href && sheets[ i ].href.indexOf( href ) > -1 ){
-        defined = true;
-      }
-    }
-    if( defined ){
-      ss.media = media || "all";
-    }
-    else {
-      setTimeout( toggleMedia );
-    }
-  }
-  toggleMedia();
-  return ss;
-}
-
 function loadJS( src, cb ){
   "use strict";
   var ref = window.document.getElementsByTagName( "script" )[ 0 ];
