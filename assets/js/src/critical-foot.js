f9d542c863c70bc481ee84771f5d75e71a25ce7d
Jonathan Suh <hello@jonsuh.com>
2015-05-05 13:55:01 -0500

    Google Analytics with new transport: beacon.

--- assets/js/src/critical-foot.js
+++ assets/js/src/critical-foot.js
@@ -2,7 +2,7 @@
 // Google Analytics
 // ==================================================
 (function(i, s, o, g, r, a, m) {
-  i['GoogleAnalyticsObject'] = r;
+  i["GoogleAnalyticsObject"] = r;
   i[r] = i[r] || function() {
     (i[r].q = i[r].q || []).push(arguments)
   }, i[r].l = 1 * new Date();
@@ -11,10 +11,10 @@
   a.async = 1;
   a.src = g;
   m.parentNode.insertBefore(a, m)
-})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+})(window, document, "script", "//www.google-analytics.com/analytics.js", "ga");
 
-ga('create', 'UA-2018785-1', 'auto');
-ga('send', 'pageview');
+ga("create", "UA-2018785-1", "auto");
+ga("send", "pageview", {transport: "beacon"});
 
 // ==================================================
 // aload
