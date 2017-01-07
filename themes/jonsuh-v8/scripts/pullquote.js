5cec93a82baec8165b81620cd9dd1e86350f49d1
Jonathan Suh <hello@jonsuh.com>
2017-01-07 11:28:43 -0600

    Custom pullquote tag plugin.

---
+++ themes/jonsuh-v8/scripts/pullquote.js
@@ -0,0 +1,30 @@
+hexo.extend.tag.register('pullquote', function(args, content) {
+  content = hexo.render.renderSync({
+    text: content,
+    engine: 'markdown'
+  });
+
+  let className = "pullquote";
+  let cite = "";
+
+  args.forEach(function(value) {
+    if (value === "wide") {
+      className += " pullquote--wide";
+    }
+
+    if (value.match(/cite:/)) {
+      value = hexo.render.renderSync({
+        text: value,
+        engine: 'markdown'
+      });
+
+      value = value.replace("cite:", "")
+                   .replace(/<\/p>/g,"")
+                   .replace(/<p>/g,"");
+
+      cite = '<cite class="pullquote-cite">' + value + '</cite>';
+    }
+  });
+
+  return '<blockquote class="' + className + '">' + content + cite + '</blockquote>';
+}, {ends: true});
