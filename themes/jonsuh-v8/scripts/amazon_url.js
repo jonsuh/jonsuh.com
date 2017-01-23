91acedb372e0fa2304aaac7ea074adcfbcc04970
Jonathan Suh <hello@jonsuh.com>
2017-01-23 12:37:11 -0600

    Config variables in site config.

--- themes/jonsuh-v8/scripts/amazon_url.js
+++ themes/jonsuh-v8/scripts/amazon_url.js
@@ -11,14 +11,7 @@ hexo.extend.tag.register('amazon_url', function(args, content) {
     amazonUrl = url.resolve("https://www.amazon.com/dp/", amazonUrl);
   }
 
-  let tagQuery = "?tag=";
-
-  if (hexo.config.amazon_tag) {
-    tagQuery += hexo.config.amazon_tag;
-  }
-  else if (hexo.theme.config.amazon_tag) {
-    tagQuery += hexo.theme.config.amazon_tag;
-  }
+  const tagQuery = "?tag=" + hexo.config.amazon_tag;
 
   return url.resolve(amazonUrl, tagQuery);
 }, {ends: false});
