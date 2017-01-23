065029a0a806857993eb19496b2204cbf9e33e3e
Jonathan Suh <hello@jonsuh.com>
2017-01-23 12:49:29 -0600

    Figure width auto by default.

--- themes/jonsuh-v8/scripts/figure.js
+++ themes/jonsuh-v8/scripts/figure.js
@@ -25,10 +25,6 @@ hexo.extend.tag.register('figure', function(args, content) {
         className += " figure--float-right";
       }
     }
-    // auto
-    if (value === "auto") {
-      className += " figure--auto";
-    }
 
     // alt
     if (value.match(/alt=/)) {
