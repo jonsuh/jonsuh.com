8e8e73376599ed90f7fd601fc164c5ec2fc9b87b
Jonathan Suh <hello@jonsuh.com>
2017-01-23 23:15:45 -0600

    page_scripts inline and uglify in production.

--- themes/jonsuh-v8/scripts/page_scripts.js
+++ themes/jonsuh-v8/scripts/page_scripts.js
@@ -2,6 +2,8 @@
   {% page_scripts yaml %}
 */
 const path = require('path');
+const fs = require('hexo-fs');
+const uglify = require('uglify-js');
 
 hexo.extend.helper.register('page_scripts', function(scripts) {
   let scriptsDir = hexo.config.scripts_dir;
@@ -11,10 +13,29 @@ hexo.extend.helper.register('page_scripts', function(scripts) {
   if (scripts !== undefined) {
     scripts.forEach(function(script) {
       if ( ! script.includes("://") && script.charAt(0) !== "/") {
-        script = path.join(scriptsDir, script);
+        if (script.includes(".inline.js")) {
+          let scriptContent = "";
+
+          script = path.join(hexo.theme_dir, '/source/', scriptsDir, script);
+
+          if (hexo.config.environment === "production") {
+            scriptContent = uglify.minify(script).code;
+          }
+          else {
+            scriptContent = fs.readFileSync(script);
+          }
+
+          script = '<script>' + scriptContent + '</script>';
+        }
+        else {
+          script = '<script src="' + path.join(scriptsDir, script) + '"></script>';
+        }
+      }
+      else {
+        script = '<script src="' + script + '"></script>';
       }
 
-      html += '<script src="' + script + '"></script>' + "\n";
+      html += script + "\n";
     });
   }
 
