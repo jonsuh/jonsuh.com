26f7aa0b3a285b1b2659c9ec1f5f5304d9439d88
Jonathan Suh <hello@jonsuh.com>
2017-01-24 14:45:19 -0600

    Flickity tag plugin.

---
+++ themes/jonsuh-v8/scripts/flickity.js
@@ -0,0 +1,26 @@
+hexo.extend.tag.register('flickity', function(args, content) {
+  let arr = content.split(/\n/);
+
+  let alt = "";
+  let src = "";
+
+  let images = "";
+
+  arr.forEach(function(image) {
+    if (image) {
+      alt = "";
+      src = image;
+
+      if (image.includes('"')) {
+        let imageSplit = image.split('"');
+
+        src = imageSplit[0].trim();
+        alt = imageSplit[1];
+      }
+
+      images += `<img src="${src}" alt="${alt}">`;
+    }
+  });
+
+  return `<div class="js-flickity" data-flickity-options='{ "imagesLoaded": true, "percentPosition": false, "pageDots": false }'>${images}</div>`;
+}, {ends: true});
