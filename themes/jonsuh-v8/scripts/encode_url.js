ec4fe501ae14330ec1f9f921db8d74a7389e5005
Jonathan Suh <hello@jonsuh.com>
2017-01-25 00:14:55 -0600

    encode_url helper.

--- themes/jonsuh-v8/scripts/encode_url.js
+++ themes/jonsuh-v8/scripts/encode_url.js
@@ -4,3 +4,10 @@
 hexo.extend.tag.register('encode_url', function(args, content) {
   return encodeURIComponent(args[0]);
 }, {ends: false});
+
+/*
+  <% encode_url('https://www.google.com') %>
+*/
+hexo.extend.helper.register('encode_url', function(args) {
+  return encodeURIComponent(args);
+});
