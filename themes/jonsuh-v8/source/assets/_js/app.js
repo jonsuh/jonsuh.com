a13adaf0d73cac8f5fec84661aebb31a6044e3d7
Jonathan Suh <hello@jonsuh.com>
2017-05-22 17:31:02 -0500

    Tweak and add home animations.

--- themes/jonsuh-v8/source/assets/_js/app.js
+++ themes/jonsuh-v8/source/assets/_js/app.js
@@ -4,11 +4,23 @@ var App = (function() {
   var init = function() {
     rellaxInit();
 
+    homeInit();
+
     Utility.domReady(function() {
       document.querySelector(".js-home__name").classList.add("is-ready");
     });
 
     Share.init();
+  };
+
+  var homeInit = function() {
+    document.querySelector(".js-home__desk-speaker-knob").addEventListener("click", function() {
+      document.querySelector(".home__desk").classList.toggle("is-speaker-off")
+    });
+
+    document.querySelector(".js-home__desk-playpause").addEventListener("click", function() {
+      document.querySelector(".home__desk").classList.toggle("is-paused")
+    });
   };
 
   var rellaxInit = function() {
